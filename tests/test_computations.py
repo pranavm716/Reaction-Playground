0eaa0c548d9976c32ef00d98a53c1d4f114037b0
Pranav <pranav.manjapra@gmail.com>
2023-06-03 22:19:41 -0700

    added test for generate_single_step_product

--- tests/test_computations.py
+++ tests/test_computations.py
@@ -1,3 +1,4 @@
+import pytest
 from rdkit import Chem
 
 from computations import (
@@ -6,24 +7,50 @@ from computations import (
     generate_unique_products,
 )
 from datatypes import Mol2dTuple
+from reaction import Reaction
+
+SmilesTuple = tuple[str, ...]
+Smiles2dTuple = tuple[tuple[str, ...], ...]
 
 
-def mol_2d_tuple_to_smiles(mol_2d_tuple: Mol2dTuple) -> tuple[tuple[str, ...], ...]:
+def mol_2d_tuple_to_smiles(mol_2d_tuple: Mol2dTuple) -> Smiles2dTuple:
     return tuple(
         tuple(Chem.MolToSmiles(s) for s in scenarios) for scenarios in mol_2d_tuple
     )
 
 
-def smiles_2d_tuple_to_mols(smiles_2d_tuple: tuple[tuple[str, ...], ...]) -> Mol2dTuple:
+def smiles_2d_tuple_to_mols(smiles_2d_tuple: Smiles2dTuple) -> Mol2dTuple:
     return tuple(
         tuple(Chem.MolFromSmiles(s) for s in scenarios) for scenarios in smiles_2d_tuple
     )
 
 
+def smiles_2d_tuples_match(tuple1: Smiles2dTuple, tuple2: Smiles2dTuple) -> bool:
+    """
+    Helper method to check if two smiles 2d tuples are the same. Takes into consideration that
+    the order of the inner and outer tuples does not matter.
+    """
+    if len(tuple1) != len(tuple2):
+        return False
+
+    set1, set2 = set(tuple1), set(tuple2)
+    for inner1 in set1:
+        inner_set1 = set(inner1)
+        for inner2 in set2:
+            inner_set2 = set(inner2)
+            if inner_set1 == inner_set2:
+                break
+        else:
+            return False
+    return True
+
+
 def test_copy_mol():
-    mol = Chem.MolFromSmiles("CCC(C#N)C1CC1")
+    smiles = "c1ccc2c(c1)c3ccccc3[nH]2"
+    mol = Chem.MolFromSmiles(smiles)
     copy = copy_mol(mol)
-    assert Chem.MolToSmiles(mol) == Chem.MolToSmiles(copy)
+    copy_smiles = Chem.MolToSmiles(copy)
+    assert Chem.CanonSmiles(smiles) == Chem.CanonSmiles(copy_smiles)
 
 
 def test_generate_unique_products():
@@ -33,8 +60,47 @@ def test_generate_unique_products():
     unique_products = generate_unique_products(products)
     unique_product_smiles = mol_2d_tuple_to_smiles(unique_products)
 
-    assert set(unique_product_smiles) == set((("N",), ("CCC#N", "CCCO"), ("C", "CCC")))
+    assert smiles_2d_tuples_match(
+        unique_product_smiles, (("N",), ("CCC#N", "CCCO"), ("C", "CCC"))
+    )
 
 
-def test_generate_single_step_product(all_reactions):
-    pass
+@pytest.mark.parametrize(
+    [
+        "reactants_smiles",
+        "reaction_index",
+        "single_step_product_smiles",
+    ],
+    [
+        [
+            "CCO",
+            2,
+            (("CC(=O)O",),),
+        ],
+        [
+            r"C/C=C/C=C(\CC)C1CC1",
+            6,
+            (("CCC(=O)C1CC1", "C/C=C/C=O"), ("CC=O", r"CC/C(=C\C=O)C1CC1")),
+        ],
+        [
+            ("CC[CH-]C1CC1", "CCC=O"),
+            12,
+            (("CCC(O)C(CC)C1CC1",),),
+        ],
+    ],
+)
+def test_generate_single_step_product(
+    reactants_smiles: str | SmilesTuple,
+    reaction_index: int,
+    single_step_product_smiles: Smiles2dTuple,
+    all_reactions: list[Reaction],
+):
+    reaction = all_reactions[reaction_index]
+    if isinstance(reactants_smiles, str):
+        reactants = Chem.MolFromSmiles(reactants_smiles)
+    elif isinstance(reactants_smiles, tuple):
+        reactants = tuple(Chem.MolFromSmiles(s) for s in reactants_smiles)
+    single_step_product = generate_single_step_product(reactants, reaction)
+    assert smiles_2d_tuples_match(
+        single_step_product_smiles, mol_2d_tuple_to_smiles(single_step_product)
+    )

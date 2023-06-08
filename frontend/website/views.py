f7f5fc189417041d1a8d71eea9bf3071b42c212e
Pranav <pranav.manjapra@gmail.com>
2023-06-08 02:33:40 -0700

    finished frontend logic for intro sequence

--- frontend/website/views.py
+++ frontend/website/views.py
@@ -8,29 +8,52 @@ from backend.program import Program
 from backend.ui import CLI
 
 views = Blueprint("views", __name__)
-program = None
 
 
-@views.route("/")
+@views.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("home.jinja")
+    def flash_invalid_mol(mol_type: str):
+        flash(
+            f"Invalid SMILES for {mol_type} molecule. Please enter valid SMILES.",
+            category="error",
+        )
 
-
-@views.route("/", methods=["POST"])
-def process_init():
-    start_mol_SMILES = request.form.get("start_mol_SMILES")
-    target_mol_SMILES = request.form.get("target_mol_SMILES")
-
-    print(start_mol_SMILES, target_mol_SMILES)
-    try:
-        start_mol = Chem.MolFromSmiles(start_mol_SMILES)
-        Chem.SanitizeMol(start_mol)
-        target_mol = Chem.MolFromSmiles(target_mol_SMILES)
-        Chem.SanitizeMol(target_mol)
-    except Exception as e:
-        flash("Invalid SMILES. Please enter valid SMILES.", category="error")
+    if request.method == "GET":
+        print("GET")
         return render_template("home.jinja")
 
+    start_mol_smiles = request.form.get("start_mol_smiles")
+    target_mol_smiles = request.form.get("target_mol_smiles")
+    print(f"{start_mol_smiles=}", f"{target_mol_smiles=}")
+
+    invalid_string = ""
+    if not start_mol_smiles:
+        invalid_string += "starting"
+
+    try:
+        start_mol = Chem.MolFromSmiles(start_mol_smiles)
+        Chem.SanitizeMol(start_mol)
+    except Exception:
+        invalid_string += "starting"
+
+    try:
+        target_mol = Chem.MolFromSmiles(target_mol_smiles)
+        Chem.SanitizeMol(target_mol)
+    except Exception:
+        invalid_string += " and target"
+
+    if invalid_string:
+        flash_invalid_mol(invalid_string)
+        return redirect(request.referrer)
+    else:
+        session["start_mol_smiles"] = start_mol_smiles
+        if target_mol_smiles:
+            session["target_mol_smiles"] = target_mol_smiles
+        return redirect(url_for("views.run_program"))
+
+
+@views.route("/run-program", methods=["GET"])
+def run_program():
     config_file_location = os.path.join(
         os.path.dirname(__file__), "../../backend/config.json"
     )
@@ -44,6 +67,12 @@ def process_init():
 
         RDLogger.DisableLog("rdApp.warning")
 
+    start_mol_smiles = session.get("start_mol_smiles")
+    start_mol = Chem.MolFromSmiles(start_mol_smiles)
+
+    target_mol_smiles = session.get("target_mol_smiles")
+    target_mol = Chem.MolFromSmiles(target_mol_smiles) if target_mol_smiles else None
+    
     program = Program(
         ui=CLI(),
         start_mol=start_mol,
@@ -53,13 +82,9 @@ def process_init():
         all_reactions_file_path=config.all_reactions_file_path,
         multiple_reactants_prompts=config.multiple_reactants_prompts,
     )
-    return redirect(url_for("views.program"))
 
-
-@views.route("/program")
-def program():
     return render_template(
-        "program.jinja",
-        start_mol_SMILES=Chem.MolToSmiles(program.start_mol),
-        target_mol_SMILES=Chem.MolToSMILES(program.target_mol),
+        "solver_mode.jinja",
+        start_mol_SMILES=start_mol_smiles,
+        target_mol_SMILES=target_mol_smiles,
     )

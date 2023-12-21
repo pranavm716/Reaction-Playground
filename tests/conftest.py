e1ef732c70ffdf49553b9aa8c9bed9ace82b650f
pranavm716 <pranav.manjapra@gmail.com>
2023-12-21 01:11:57 -0800

    fixing test paths

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,7 +7,11 @@ import pathlib
 
 @pytest.fixture
 def all_reactions() -> list[Reaction]:
-    all_reactions_file_path = pathlib.Path("../website") / ALL_REACTIONS_FILE_PATH
+    all_reactions_file_path = (
+        pathlib.Path(__file__).resolve().parent.parent
+        / "website"
+        / ALL_REACTIONS_FILE_PATH
+    )
     real_reactions = read_all_reactions_from_file(all_reactions_file_path)
 
     made_up_reaction = Reaction(

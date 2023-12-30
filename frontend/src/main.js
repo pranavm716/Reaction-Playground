3efc7057cf85ba8e2bb49a81dcaee4518b33407a
pranavm716 <pranav.manjapra@gmail.com>
2023-12-30 15:29:13 -0800

    messing around with backend api calls (axios)

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:5000'
 
 createApp(App).use(router).mount('#app')

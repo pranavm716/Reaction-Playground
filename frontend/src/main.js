8997d5f641d61a320fc3634e38edd4688176cd63
pranavm716 <pranav.manjapra@gmail.com>
2024-01-02 02:54:15 -0800

    adding axios settings to main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import axios from 'axios'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:5000'
 
 loadFonts()
 

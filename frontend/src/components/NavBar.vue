c96c17fb90756e133df228b5fa7c088c94033b85
pranavm716 <pranav.manjapra@gmail.com>
2024-01-01 19:33:59 -0800

    adding more icons and styles to the navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -2,13 +2,24 @@
   <v-app-bar color="#2980B9" class="nav-bar" :elevation="4">
     <v-app-bar-nav-icon icon="mdi-react" @click="redirectHome"></v-app-bar-nav-icon>
     <v-app-bar-title class="nav-bar-title" @click="redirectHome">Reaction Playground</v-app-bar-title>
+
+    <!--    Add the nav bar route buttons-->
     <v-btn v-for="item in navBarItems" :key="item.text" :prepend-icon="item.icon" variant="text" :to="{ name: item.routeName }">
       {{ item.text }}
     </v-btn>
-
+    <v-divider
+        class="mx-3 align-self-center"
+        length="24"
+        thickness="2"
+        vertical
+    ></v-divider>
+    <v-btn icon>
+      <v-icon>mdi-cog</v-icon>
+    </v-btn>
     <v-btn icon>
       <v-icon>mdi-help-circle</v-icon>
     </v-btn>
+
   </v-app-bar>
 </template>
 
@@ -37,5 +48,10 @@ export default defineComponent({
 <style>
 .v-app-bar-title {
   cursor: pointer;
+}
+.v-btn--active {
+  background-color: transparent;
+  color: inherit;
+  border-bottom: 2px solid white; /* Set the color and style of the underline */
 }
 </style>

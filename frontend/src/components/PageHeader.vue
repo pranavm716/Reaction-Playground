91f36d46639a89956a10a9c5790ad14e103529e2
pranavm716 <pranav.manjapra@gmail.com>
2024-01-03 00:49:03 -0800

    created separate component for page header

---
+++ frontend/src/components/PageHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container class="ma-4 pa-4">
+    <h1 :class="`d-inline-block text-h4 font-weight-medium text-${$route.meta.navBarColor}`">{{ highlightedWord }}</h1>
+    <h1 class="d-inline-block text-h4 font-weight-light">{{ otherWords }}</h1>
+  </v-container>
+</template>
+
+<script>
+
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageHeader',
+  props: ['highlightedWord', 'otherWords']
+})
+</script>

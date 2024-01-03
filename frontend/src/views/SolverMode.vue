ca3e2a41fb8deae244953fadc00e48ed0561821b
pranavm716 <pranav.manjapra@gmail.com>
2024-01-03 00:51:03 -0800

    filled in page template for solver mode

--- frontend/src/views/SolverMode.vue
+++ frontend/src/views/SolverMode.vue
@@ -1,7 +1,27 @@
 <template>
-  <p>solver mode</p>
+  <PageHeader highlighted-word="Solver" other-words="Mode"/>
+  <ChemDrawTool @smiles="storeSmiles"/>
+
 </template>
 
 <script>
 
+import ChemDrawTool from '@/components/ChemDrawTool.vue'
+import PageHeader from '@/components/PageHeader.vue'
+
+export default {
+  name: 'SolverMode',
+  components: { ChemDrawTool, PageHeader },
+  data () {
+    return {
+      smiles: null
+    }
+  },
+  methods: {
+    storeSmiles (smiles) {
+      this.smiles = smiles
+    }
+  }
+
+}
 </script>

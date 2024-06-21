b8680237ef832b2e3984e76f1e2fc58784970082
Pranav <pranav.manjapra@gmail.com>
2024-06-21 11:41:26 -0700

    handled clicking of multiple products

--- frontend/src/components/MolImage.js
+++ frontend/src/components/MolImage.js
@@ -1,8 +1,11 @@
-const MolImage = ({smiles, encoding}) => {
-    // TODO: Add context menu
+const MolImage = ({smiles, encoding, setSmiles}) => {
+    // Supports both clickable and non clickable mol images
+    const className = setSmiles ? "clickable-mol-image" : "mol-image";
     return (
-        <img className="mol-image" width={250} height={250} src={`data:image/png;base64,${encoding}`} alt={smiles} />
+        <img onClick={() => setSmiles(smiles)} className={className} width={200} height={200} src={`data:image/png;base64,${encoding}`} alt={smiles} />
     );
+    
+    // TODO: Add context menu on right click
 }
  
 export default MolImage;
--- frontend/src/components/PlaygroundStepProductPicker.js
+++ frontend/src/components/PlaygroundStepProductPicker.js
@@ -1,7 +1,16 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import MolImage from "./MolImage";
+
+const plusIcon = <svg style={{
+    padding: "0 10px",
+}} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000">
+    <path d="M450-450H220v-60h230v-230h60v230h230v60H510v230h-60v-230Z" />
+</svg>
+
 
 const PlaygroundStepProductPicker = ({ products, setSmiles, reactionName }) => {
     useEffect(() => {
+        // If there is only one product, there is no choice the user has to make
         if (products.length === 1) {
             setSmiles(products[0].smiles);
         }
@@ -9,6 +18,7 @@ const PlaygroundStepProductPicker = ({ products, setSmiles, reactionName }) => {
 
     return (
         <>
+            {/* down arrow + reaction name */}
             <div style={{
                 position: 'relative',
                 display: 'flex',
@@ -19,13 +29,18 @@ const PlaygroundStepProductPicker = ({ products, setSmiles, reactionName }) => {
                 <span style={{ width: 200, position: 'absolute', left: 60 }}>{reactionName}</span>
             </div>
 
-            {products.length > 1 && products.map(product => (
-                // TODO: change to image and generalize MolImage to handle clicks
-                <div key={product.smiles}>
-                    <button onClick={() => setSmiles(product.smiles)}>{product.smiles}</button>
-                </div>
-            ))}
-            <p>This produces {products.length} products. Click on the one you want to analyze next.</p>
+            {/* list of clickable products */}
+            <div className="mol-row">
+                {products.length > 1 && products.map((product, index) => (
+                    <React.Fragment key={product.smiles}>
+                        <MolImage smiles={product.smiles} encoding={product.encoding} setSmiles={setSmiles} />
+                        {index < products.length - 1 && plusIcon}
+                    </React.Fragment>
+                ))}
+            </div>
+            <p className="multi-product-text">
+                This produces {products.length} products. Click on the one you want to analyze next.
+            </p>
         </>
     );
 }

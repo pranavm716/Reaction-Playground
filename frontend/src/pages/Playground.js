be555f1e0cd8de35ccb18c2b48c04d7ab5f2421c
Pranav <pranav.manjapra@gmail.com>
2024-06-22 04:52:11 -0700

    message tweak

--- frontend/src/pages/Playground.js
+++ frontend/src/pages/Playground.js
@@ -104,7 +104,7 @@ const Playground = () => {
         <>
             {
                 !stepMetadata && <p>
-                    Welcome to Playground Mode! Here, you can draw a molecule and
+                    Welcome to Reaction Playground! In playground mode, you can draw a molecule and
                     experiment with running common organic reactions on it.
                 </p>
             }

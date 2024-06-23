0b1638fcdf8b3eaae485aa400ced2e714d4d4e13
Pranav <pranav.manjapra@gmail.com>
2024-06-22 22:07:09 -0700

    dynamic route styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Navbar from "./components/NavBar";
 import Playground from "./pages/Playground";
 import Solver from "./pages/Solver";
 import Classifier from "./pages/Classifier";
+import { useEffect } from "react";
+
+const primaryColorCSSTag = "--primary-color";
+
+const useUpdatePrimaryColor = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const rootStyle = document.documentElement.style;
+    switch (location.pathname) {
+      case "/":
+        rootStyle.setProperty(primaryColorCSSTag, "#f1356d");
+        break;
+      case "/solver":
+        rootStyle.setProperty(primaryColorCSSTag, "#429987");
+        break;
+    }
+  }, [location]);
+}
 
 const App = () => {
+  useUpdatePrimaryColor();
+
   return (
     <>
       <Navbar />

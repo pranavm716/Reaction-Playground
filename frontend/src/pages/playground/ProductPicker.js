70b8d3c6e88c04c245f514170390013cc09cae20
pranavm716 <pranav.manjapra@gmail.com>
2024-06-25 18:31:51 -0700

    removing unused import

--- frontend/src/pages/playground/ProductPicker.js
+++ frontend/src/pages/playground/ProductPicker.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import MolImage from "../../components/MolImage";
 import { ArrowWithReactionInfo, plusIcon } from "../../components/SmallUIComponents";
 
